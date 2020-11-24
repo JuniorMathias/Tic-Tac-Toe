1fed2f1e1a3c533594dc4071979eb2d5b87e5ed1
Jorge Matias <juniormathiias@gmail.com>
2020-11-24 19:06:27 +0000

    My first commit, and I added props, css and passed a the main functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const Square = (props) => {
+
+  return (
+    <button 
+    className="square"
+    onClick={() => {}}>
+      {props.value}
+    </button>
+
+  );
+};
+
+
+const Board = () => {
+  const initialSquares = [ // passando os valores iniciais para os quadrados onde serão nulos
+    null,null,null,
+    null,null,null,
+    null,null,null
+];
+
+
+  const [squares,setSquares] = useState(initialSquares);
+
+    const renderSquare = (i) => {
+      return (
+        <Square value={squares[i]}/> // passando o valor do array square e recebendo i
+      );
+    }
+
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20,
+    }}>
+
+      Board
+      <div className="board-row">
+       {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className="board-row">
+       {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className="board-row">
+       {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+    </div>
+
+  )
+};
+const Game = () => {
+  return (
+    <div className=" game" >
+      Game
+      <Board />
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
